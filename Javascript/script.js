1b83f4cf2d8212e27461dcda0941a90e2d3bc51b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-22 12:28:25 +0600

    script   :bulb:

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,7 +3,7 @@ function changeText() {
   document.getElementById("main-heading").innerText = "Text Changed by JavaScript!";
 }
 
-// 2. Add a new paragraph dynamically
+// 2. Add a new paragraph Dynamically
 function addElement() {
   const newPara = document.createElement("p");
   newPara.innerText = "This is a new paragraph added with JS!";
